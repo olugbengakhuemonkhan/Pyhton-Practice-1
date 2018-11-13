3314645cf4bc05f82a6e270bdd15c967063cffe0
Akhuemonkhan Olugbenga <gbengaloveeyesimages@gmail.com>
2018-11-13 12:13:00 -0500

    My first Commit

---
+++ python101.py
@@ -0,0 +1,19 @@
+print "Hello, World";
+print "Hello, World";
+
+print """
+    It happend again.
+    He killed him agin
+    So we ran to the scene
+"""
+
+# Variables
+# -strings, letters, numbers, or any oher stuff
+# you can make with a keyboard
+
+# a variable is just a fast way to refer to something else
+# variable do not make the program faster.
+# it makes it slower
+
+theBestClass = "the 11-18 immersive"
+print theBestClass
