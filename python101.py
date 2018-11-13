f4ed42aaea7bb6a53bd48905319707a565d4395c
Akhuemonkhan Olugbenga <gbengaloveeyesimages@gmail.com>
2018-11-13 14:35:32 -0500

    Newer

--- python101.py
+++ python101.py
@@ -1,55 +1,89 @@
-print "Hello, World";
-print "Hello, World";
+# print "Hello, World";
+# print "Hello, World";
 
-print """
-    It happend again.
-    He killed him agin
-    So we ran to the scene
-"""
+# print """
+#     It happend again.
+#     He killed him agin
+#     So we ran to the scene
+# """
 
-# Variables
-# -strings, letters, numbers, or any oher stuff
-# you can make with a keyboard
+# # Variables
+# # -strings, letters, numbers, or any oher stuff
+# # you can make with a keyboard
 
-# a variable is just a fast way to refer to something else
-# variable do not make the program faster.
-# it makes it slower
+# # a variable is just a fast way to refer to something else
+# # variable do not make the program faster.
+# # it makes it slower
 
-# theBestClass = "the 11-18 immersive"
-# print theBestClass
-# Programming languages see different types of variables differently
-# -String -English stuff.
-# - Number- I think you know what this is. Something with numbers (or -or else)
-# print 3.3e10+"Joe"
-# -- float = it has a . in it
-# -- integer = it has no .
+# # theBestClass = "the 11-18 immersive"
+# # print theBestClass
+# # Programming languages see different types of variables differently
+# # -String -English stuff.
+# # - Number- I think you know what this is. Something with numbers (or -or else)
+# # print 3.3e10+"Joe"
+# # -- float = it has a . in it
+# # -- integer = it has no .
   
 
 
-#    print theBestClass
-# Programming languages see different types of variables differently
-# -String -English stuff.
-# - Number- I think you know what this is. Something with numbers (or -or else)
-# print 3.3e10+"Joe"
-# -- float = it has a . in it
-# -- integer = it has no .
-#ish stuff.
-# - Number- I think you know what this is. Something with numbers (or -or else)
-# print 3.3e10+"Joe"
-# -- float = it has a . in it
-# -- integer = it has no 
-
-month = "November";
-print type(month)
-date = 13
-print type(date)
-dateAsFloat = 13.0
-print type(dateAsFloat)
-aBool = True
-print type(aBool)
-aList = []
-print type(aList)
-aDictionary = {}
-print type(aDictionary)
+# #    print theBestClass
+# # Programming languages see different types of variables differently
+# # -String -English stuff.
+# # - Number- I think you know what this is. Something with numbers (or -or else)
+# # print 3.3e10+"Joe"
+# # -- float = it has a . in it
+# # -- integer = it has no .
+# #ish stuff.
+# # - Number- I think you know what this is. Something with numbers (or -or else)
+# # print 3.3e10+"Joe"
+# # -- float = it has a . in it
+# # -- integer = it has no 
 
 
+
+# # This is how it works
+# # while stile learning
+
+
+# month = "November";
+# print type(month)
+# date = 13
+# print type(date)
+# dateAsFloat = 13.0
+# print type(dateAsFloat)
+# aBool = True
+# print type(aBool)
+# aList = []
+# print type(aList)
+# aDictionary = {}
+# print type(aDictionary)
+
+# concatenate is programming speak for add things tigether
+# first = "Olugbenga"
+# last = "Akhuemonkhan"
+# fullName = first + last;
+# fullName = first + " " + last;
+
+# print fullName
+
+# fourteen 10 + 4
+# print fourteen
+# fourteen = "10" + "4"
+# print fourteen
+
+# # cast = change a variable to a new data type
+# # fourteen = int("10") + 4
+
+# Math = +, -, *, %
+print 2+2
+print 2-2
+print 2*2
+print 2%2
+print 2%3
+print 2**3
+print "-" * 20
+# A string and a * and a number = give me X strings
+
+# python does not have a simple incrementer
+# num = 1;
+# # num ++
