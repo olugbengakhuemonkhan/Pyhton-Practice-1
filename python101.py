cfdebd975b34dea1883697a649366367885d50c9
Akhuemonkhan Olugbenga <gbengaloveeyesimages@gmail.com>
2018-11-13 16:07:00 -0500

    I am here

--- python101.py
+++ python101.py
@@ -7,42 +7,42 @@
 #     So we ran to the scene
 # """
 
-# # Variables
-# # -strings, letters, numbers, or any oher stuff
-# # you can make with a keyboard
+# # # Variables
+# # # -strings, letters, numbers, or any oher stuff
+# # # you can make with a keyboard
 
-# # a variable is just a fast way to refer to something else
-# # variable do not make the program faster.
-# # it makes it slower
+# # # a variable is just a fast way to refer to something else
+# # # variable do not make the program faster.
+# # # it makes it slower
 
-# # theBestClass = "the 11-18 immersive"
-# # print theBestClass
-# # Programming languages see different types of variables differently
-# # -String -English stuff.
-# # - Number- I think you know what this is. Something with numbers (or -or else)
-# # print 3.3e10+"Joe"
-# # -- float = it has a . in it
-# # -- integer = it has no .
+# theBestClass = "the 11-18 immersive"
+# print theBestClass
+# # # Programming languages see different types of variables differently
+# # # -String -English stuff.
+# # # - Number- I think you know what this is. Something with numbers (or -or else)
+# # # print 3.3e10+"Joe"
+# # # -- float = it has a . in it
+# # # -- integer = it has no .
   
 
 
-# #    print theBestClass
-# # Programming languages see different types of variables differently
-# # -String -English stuff.
-# # - Number- I think you know what this is. Something with numbers (or -or else)
-# # print 3.3e10+"Joe"
-# # -- float = it has a . in it
-# # -- integer = it has no .
-# #ish stuff.
-# # - Number- I think you know what this is. Something with numbers (or -or else)
-# # print 3.3e10+"Joe"
-# # -- float = it has a . in it
-# # -- integer = it has no 
+# # #    print theBestClass
+# # # Programming languages see different types of variables differently
+# # # -String -English stuff.
+# # # - Number- I think you know what this is. Something with numbers (or -or else)
+# # # print 3.3e10+"Joe"
+# # # -- float = it has a . in it
+# # # -- integer = it has no .
+# # #ish stuff.
+# # # - Number- I think you know what this is. Something with numbers (or -or else)
+# # # print 3.3e10+"Joe"
+# # # -- float = it has a . in it
+# # # -- integer = it has no 
 
 
 
-# # This is how it works
-# # while stile learning
+# # # This is how it works
+# # # while stile learning
 
 
 # month = "November";
@@ -53,12 +53,12 @@
 # print type(dateAsFloat)
 # aBool = True
 # print type(aBool)
-# aList = []
-# print type(aList)
-# aDictionary = {}
-# print type(aDictionary)
+# # aList = []
+# # print type(aList)
+# # aDictionary = {}
+# # print type(aDictionary)
 
-# concatenate is programming speak for add things tigether
+# # concatenate is programming speak for add things tigether
 # first = "Olugbenga"
 # last = "Akhuemonkhan"
 # fullName = first + last;
@@ -66,24 +66,59 @@
 
 # print fullName
 
-# fourteen 10 + 4
+# fourteen = 10 + 4
 # print fourteen
 # fourteen = "10" + "4"
 # print fourteen
 
-# # cast = change a variable to a new data type
-# # fourteen = int("10") + 4
+# # # cast = change a variable to a new data type
+# # # fourteen = int("10") + 4
 
-# Math = +, -, *, %
-print 2+2
-print 2-2
-print 2*2
-print 2%2
-print 2%3
-print 2**3
-print "-" * 20
-# A string and a * and a number = give me X strings
+# # Math = +, -, *, %
+# print 2+2
+# print 2-2
+# print 2*2
+# print 2%2
+# print 2%3
+# print 2**3
+# print "-" * 20
+# # A string and a * and a number = give me X strings
 
-# python does not have a simple incrementer
-# num = 1;
-# # num ++
+# # python does not have a simple incrementer
+# # num = 1;
+# # # num ++
+
+# # Math = +, -, /, *, %
+# print 2+2
+# # print 2-2
+# # print 2/2
+# # print 2*2
+
+# name = raw_input("What is your name ? ")
+# print name
+
+# conditionals
+# a single = sign means set the left to whatever is on the right
+# two = signs, means compare what's on the left to whatever is on the right
+# print 2 == 2
+# print 2 == 1
+# print 2 == "2"
+secret_number = 5;
+if(secret_number ==3):
+    print "secret number "
+    secret_number = 5;
+if(secret_number == 3):
+    print "Secret number is 3";
+else:
+    print "Secret number is not 3.";
+
+game_on = True;
+i = 0;
+# while(game_on):
+while(game_on == True):
+    i+= 1
+    if(i == 10):
+        game_on = False
+    else:
+        print "Game on!!"
+print "Loop exited!"
